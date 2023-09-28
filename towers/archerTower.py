142b90ce325154ef2dc4774520e6142915ef3a82
KiedusCracknell <kieduscracknell@gmail.com>
2023-09-28 23:33:39 +1300

    Filled some basics in the archerTower class!

--- towers/archerTower.py
+++ towers/archerTower.py
@@ -2,5 +2,23 @@ import pygame
 from towers.tower import Tower
 
 class ArcherTower(Tower):
-    pass
+    def __init__(self,x,y):
+        super().__init__(self,x,y)
+        
+        self.tower_imgs = []
+        self.archer_imgs = []
+        self.archer_count = 0
+        
+    def draw(self, win):
+        super().draw(win)
+    
+    def attacl(self, enemies):
+        """
+        attacks an enemy in the enemy list, modifies the list
+        :param enemies: enemy list
+        :return: None
+        """
+        
+            
+    
     
