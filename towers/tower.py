0d72cf72d1e9982dbb3eb521f8f6fa4b277cf87b
KiedusCracknell <kieduscracknell@gmail.com>
2023-09-28 23:25:15 +1300

    Filled in basic functions for tower abstract class

--- towers/tower.py
+++ towers/tower.py
@@ -14,22 +14,59 @@ class Tower:
         self.level = 1
         self.selected = False
         self.menu = None
-        self.imgs
+        self.tower_imgs
         
         
     
     def draw(self, win):
-        pass
+        """
+        draws the tower
+        :param win: surface
+        :return: None
+        """
+        img = self.tower_imgs[self.level - 1]
+        win.blit(img, self.x-img.get_width()//2, self.y-img.get_height()//2)
     
-    def click(self):
-        pass
+    def click(self,X,Y):
+        """
+        returns if the tower has been clicked on and selects tower if it was clicked
+        :param X: int
+        :param Y: int
+        :return: Bool
+        """
+        if X <= self.x + self.width and X >= self.x:
+            if Y <= self.y + self.height and Y >= self.y:
+                return True
+        return False
     
     def sell(self):
-        pass
+        """
+        call to sell the selected tower, return sell price
+        :return: int
+        """
+        return self.sell_price[self.level-1]
     
     def upgrade(self):
-        pass
+        """
+        upgrades the selected tower for a given cost
+        :return: None
+        """
+        self.level += 1
     
-    def move(self):
-        pass
+    def get_upgrade_cost(self):
+        """
+        returns the upgrade cost for the selected tower, if 0 then upgrade isn't available
+        :return: int
+        """
+        return self.price[self.level-1]
+    
+    def move(self,x,y):
+        """
+        Makes tower follow the mouse position when placing a new tower
+        :param x: int
+        :param y: int
+        :return: None
+        """
+        self.x = x
+        self.y = y 
     
